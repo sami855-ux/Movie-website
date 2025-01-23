302d7326710b281d5e78657927b8527de9e89ca8
sami855-ux <samitale56@outlook.com>
2025-01-23 19:20:52 +0500

    Hero section is added to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.tsx"
+import Hero from "./components/Hero.tsx"
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
