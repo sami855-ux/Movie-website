fd046bc1ff1d34c049c7c2923c1ec5e8c064884c
sami855-ux <samitale56@outlook.com>
2025-03-05 16:36:16 +0500

    Page route is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import Header from "./components/Header.tsx"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import Hero from "./components/Hero.tsx"
+import AppLayout from "./ui/AppLayout.tsx"
+import Movie from "./Pages/movie/Movie.tsx"
+import Tvshow from "./Pages/Tvshow/Tvshow.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Hero />,
+      },
+      {
+        path: "movie",
+        element: <Movie />,
+      },
+      {
+        path: "tvshow",
+        element: <Tvshow />,
+      },
+    ],
+  },
+])
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
