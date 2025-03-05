8ba1d40ac0d06aae51b4b9f8acba39c20746b78b
sami855-ux <samitale56@outlook.com>
2025-03-05 13:35:13 +0500

    Function for fetchhing an image is added

--- src/api/imageFetch.ts
+++ src/api/imageFetch.ts
@@ -1,17 +1,17 @@
 const BASE_URL = "https://www.omdbapi.com/?apikey=a4650e89&"
 
-const fetchImage = async (
-  onMovieData: (poster: string) => void
-): Promise<void> => {
+const fetchImage = async (name: string): Promise<string> => {
+  let dataImg
   try {
-    const response = await fetch(`${BASE_URL}t=batman`)
+    const response = await fetch(`${BASE_URL}t=${name}`)
     const data = await response.json()
 
-    onMovieData(data.poster)
-    console.log(data)
+    dataImg = data.poster
   } catch (error) {
     console.error(error)
   }
+
+  return dataImg
 }
 
 export { fetchImage }
